6137eb9e460001460fb9455eff98d90508766e6e
vishwas gautam <vgvishwas1101@gmail.com>
2021-01-13 10:24:36 +0530

    *

--- KF_in_microgrids.m
+++ KF_in_microgrids.m
@@ -1,7 +1,7 @@
 A = [175.9, 176.8, 511, 103.6; -350, 0, 0, 0; -544.2, -474.8, -408.8, -828.8; -119.7, -554.6, -968.8, -1077.5];
 B = [0.8, 334.2, 525.1, -103.6; -350, 0, 0, 0; -69.3, -66.1, -420.1, -828.8; -434.9, -414.2, -108.7, -1077.5];
 %C = ? Randomly taken here
-C = [1, 0, 0, 0];
+C = [1, 1, 1, 1];
 D = 0;
 
 step_size = 0.00001;
@@ -18,4 +18,17 @@ cov_w = cov(w);
 
 sys = ss(A_d, B_d, C, D, -1);
 
-[K, L, P, M] = kalman(sys, cov_n, cov_w);
+[KF, L, P, M] = kalman(sys, cov_n, cov_w);
+
+KF = KF(1,:);
+
+disp(M); %innovation gain 
+
+%Building the plant model and putting it parallel to the estimated model
+a = A;
+b = [B B 0*B 0*B];
+c = [C ;C];
+d = [0 0 0 0; 0 0 0 0]
+
+
+
---
+++ filterdesign.m
@@ -0,0 +1,56 @@
+A = [1.1269, -0.4940, 0.1129; 1.0000, 0, 0; 0, 1.0000, 0];
+
+B = [-0.3832
+      0.5919
+      0.5191];
+
+C = [1 0 0];
+
+D = 0;
+
+Plant = ss(A,[B B],C,0,-1,'inputname',{'u' 'w'},'outputname','y');
+
+Q = 2.3; %noise covariances
+R = 1;
+
+[kalmf,L,~,M,Z] = kalman(Plant,Q,R);
+
+kalmf = kalmf(1,:);
+
+disp(M);
+
+a = A;
+b = [B B 0*B];
+c = [C;C];
+d = [0 0 0;0 0 1];
+P = ss(a,b,c,d,-1,'inputname',{'u' 'w' 'v'},'outputname',{'y' 'yv'});
+
+sys = parallel(P,kalmf,1,1,[],[]);
+
+SimModel = feedback(sys,1,4,2,1);
+SimModel = SimModel([1 3],[1 2 3]);     % Delete yv form I/O
+
+SimModel.inputname
+SimModel.outputname
+
+t = (0:100)';
+u = sin(t/5);
+
+rng(10,'twister');
+w = sqrt(Q)*randn(length(t),1);
+v = sqrt(R)*randn(length(t),1);
+
+out = lsim(SimModel,[w,v,u]);
+
+y = out(:,1);   % true response
+ye = out(:,2);  % filtered response
+yv = y + v;     % measured response
+
+clf
+subplot(211), plot(t,y,'b',t,ye,'r--'),
+xlabel('No. of samples'), ylabel('Output')
+title('Kalman filter response')
+subplot(212), plot(t,y-yv,'g',t,y-ye,'r--'),
+xlabel('No. of samples'), ylabel('Error')
+
+
