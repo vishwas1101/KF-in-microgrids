34b7eb709fa8dd65a6a3984842e84b6bfdec8380
vishwas gautam <vgvishwas1101@gmail.com>
2021-01-22 23:16:08 +0530

    *

--- outputestimate_3var.m
+++ outputestimate_3var.m
@@ -5,18 +5,19 @@ A = [1.1269, -0.4940, 0.1129; 1.0000, 0, 0; 0, 1.0000, 0];
 
 B = [-0.3832; 0.5919; 0.5191];
 
-C = [1, 0, 0];
+C = [0, 1, 0];
 
 D = 0;
 
-Plant = ss(A,[B B],C,0,-1,'inputname',{'u' 'w'},'outputname','y')
+Plant = ss(A,[B B],C,0,-1,'inputname',{'u' 'w'},'outputname','y');
 
 Q = 2.3; %noise covariances
 R = 1;
 
-[kalmf,L,~,M,Z] = kalman(Plant,Q,R)
+[kalmf,L,~,M,Z] = kalman(Plant,Q,R);
 
-kalmf = kalmf(1,:);
+
+% kalmf = kalmf(1,:);
 
 %disp(M);
 
@@ -24,34 +25,36 @@ a = A;
 b = [B B 0*B];
 c = [C;C];
 d = [0 0 0;0 0 1];
+
 P = ss(a,b,c,d,-1,'inputname',{'u' 'w' 'v'},'outputname',{'y' 'yv'});
 
-sys = parallel(P,kalmf,1,1,[],[]);
+sys = parallel(P,kalmf,1,1,[],[])
 
 %sys(1,4)
-SimModel = feedback(sys,1,4,2,1);    %for MIMO system mention the inputs and outputs
-SimModel = SimModel([1 3],[1 2 3]);    % Delete yv form I/O
+SimModel = feedback(sys,1,4,2,1);   %for MIMO system mention the inputs and outputs
+SimModel = SimModel([1 3 4 5 6],[1 2 3]); % Delete yv form I/O
 
-SimModel.inputname
-SimModel.outputname
+SimModel.inputname;
+SimModel.outputname;
 
 t = (0:100)';
 
-
 rng(1,'twister');
 
-u = -t/2;
+u = 0.0001*randn(length(t),1);
 w = randn(length(t),1);
 v = randn(length(t),1);
 
 out = lsim(SimModel,[w,v,u]);
+out2 = lsim(P,[w,v,u]);
 
 y = out(:,1);   % true response
 ye = out(:,2);  % filtered response
+x1_e = out(:,4);
 yv = y + v;     % measured response
 
 clf
-subplot(111), plot(t,y,'g',t,ye,'b'),
+subplot(111), plot(t,y,'g',t,ye,'b',t, x1_e,"r"),
 xlabel('No. of samples'), ylabel('Output')
 title('Kalman filter response')
 
--- stateestimate_4var.m
+++ stateestimate_4var.m
@@ -6,7 +6,7 @@ B = [0.8, 334.2, 525.1, -103.6; -350, 0, 0, 0; -69.3, -66.1, -420.1, -828.8; -43
 C = [1, 0, 1, 0];
 D = 0;
 
-step_size = 0.00001;
+step_size = .00001;
 I = eye(4,4);
 
 A_d = I + A*step_size;
@@ -24,7 +24,7 @@ plant = ss(A_d, B_d, C, D, -1, 'inputname', {'u' 'v' 'w' 'x'}, 'outputname', 'y'
 
 %the 2,3,4,5 groups are the state estimates
 %so we keep only that 
-KF = KF([2 3 4 5],:); 
+% KF = KF([2 3 4 5],:); 
 
 
 %disp(M); %innovation gain 
@@ -34,16 +34,35 @@ a = A;
 b = [0.8, 334.2, 525.1, -103.6, 0; -350, 0, 0, 0, 0; -69.3, -66.1, -420.1, -828.8, 0; -434.9, -414.2, -108.7, -1077.5, 0];
 c = [C ;C];
 d = [0, 0, 0, 0, 0; 0, 0, 0, 0, 1];
- 
-Plant1 = ss(a, b, c, d, -1, 'inputname', {'u', 'v', 'w', 'x', 'z'}, 'outputname', {'y','yv'});
+
+Plant1 = ss(a, b, c, d, -1, 'inputname', {'u' 'v' 'w' 'x' 'z'}, 'outputname', {'y' 'yv'});
 
 % no we put it in parallel with the estimated block
-sys = parallel(Plant1, KF, 1, 1, [], []);
+sys = parallel(Plant1, KF, 1, 1, [], [])
 
-simModel = feedback(sys, 1, 6, 2, 1);
-simModel = simModel([1 3 4 5 6], [1 2 3 5]);
+simModel = feedback(sys, 1, 8, 2, 1);
+simModel = simModel([1 3 4 5 6 7], [3 4 5 6 7]);
 
 simModel.inputname;
 simModel.outputname;
 
 %now plot the results 
+% t = (0:100)';
+% 
+% rng(10,'twister');
+% 
+% u = randn(length(t),1);
+% w = randn(length(t),1);
+% v = randn(length(t),1);
+% x = randn(length(t),1);
+% z = randn(length(t),1);
+% 
+% out = lsim(simModel,[x, z, u, v, w]);
+% 
+% y = out(:,1);   % true response
+% ye = out(:,2);  % filtered response
+% 
+% clf
+% subplot(111), plot(t,y,'g',t,ye,'b'),
+% xlabel('No. of samples'), ylabel('Output')
+% title('Kalman filter response')
